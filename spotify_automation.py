0cb40d2c762ab578f7ad373e290de601650359a7
rebridge <63208511+rebridge@users.noreply.github.com>
2020-05-05 20:34:29 -0400

    Add files via upload

--- spotify_automation.py
+++ spotify_automation.py
@@ -2,12 +2,11 @@
 
 import time
 
-print('Enter username: ')
-userlogin = str(input())
-print('Enter password: ')
-passwordlogin = str(input())
-time.sleep(1)
-
+#print('Enter username: ')
+#userlogin = str(input())
+#print('Enter password: ')
+#passwordlogin = str(input())
+#time.sleep(1)
 
 import selenium
 import pyautogui
@@ -15,6 +14,7 @@ from selenium import webdriver
 
 
 PATH = "/users/rebridge/Spotify Automation/chromedriver"
+
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://accounts.spotify.com/en/authorize?client_id=09793e3afafa4797bf853f2ad5db72f3&redirect_uri=http://107.170.81.187:8080/SpotifyLoginCallback/&response_type=code&scope=user-read-recently-played+user-library-read+user-top-read&show_dialog=false&state=34fFs29kd09")
@@ -23,13 +23,11 @@ driver.get("https://accounts.spotify.com/en/authorize?client_id=09793e3afafa4797
 
 
 username = driver.find_element_by_id('login-username')
-username.send_keys(userlogin)
+username.send_keys("lincbradthom")
 time.sleep(1)
 
-
-
 password = driver.find_element_by_id('login-password')
-password.send_keys(passwordlogin)
+password.send_keys("fortnitepro69")
 time.sleep(1)
 
 login = driver.find_element_by_id('login-button')
@@ -38,11 +36,12 @@ login.click()
 #if statement for first time (shoould only fire if certain url is active)
 #this section is onlt needed for a new log in
 
-#time.sleep(3)
-#agree = driver.find_element_by_id('auth-accept')
-#agree.click()
-
-time.sleep(7)
+if driver.current_url == 'https://accounts.spotify.com/en/authorize?client_id=09793e3afafa4797bf853f2ad5db72f3&redirect_uri=http:%2F%2F107.170.81.187:8080%2FSpotifyLoginCallback%2F&response_type=code&scope=user-read-recently-played%20user-library-read%20user-top-read&show_dialog=false&state=34fFs29kd09' :
+    time.sleep(3)
+    agree = driver.find_element_by_id('auth-accept')
+    agree.click()
+else :
+    time.sleep(7)
 
 #refreshes webpage
 pyautogui.keyDown('command')
@@ -57,9 +56,6 @@ time.sleep(5)
 #pyautogui.write('Top tracks -- Short Term')
 #time.sleep(2)
 
-#pyautogui.press('down', presses=90)
-
-tracks = driver.find_element_by_xpath('/html/body/ol[4]')
-print(str(tracks))
-
-songs = [tracks]
+tracks_element = driver.find_element_by_xpath('/html/body/ol[4]')
+tracks = tracks_element.text
+print(tracks)
